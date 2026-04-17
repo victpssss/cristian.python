c1cc3c74bc03cc6b8487dd64c25ba8c7879daf44
victpssss <vice.can27@gmail>
2026-04-17 18:17:27 -0400

    ejemplos for

---
+++ cristian.py
@@ -0,0 +1,36 @@
+# suma=0
+# cant=int(input("cuantas notas sacaste: "))
+# for i in range(cant):
+#     nota=float(input("cual fue su nota: "))
+#     suma=suma+nota
+# prom=suma/cant
+# print(f"este es su promedio {prom}")
+
+# n=int(input("ingrese el numero: "))
+# can=1
+# for i in range(n):
+#     can=can*(i+1)
+# print(f"la suma toria es {can}")
+
+# contador de vocales
+# letra=input("ingrese su nombre: ")
+# less=0
+# vocales=0
+# lolos=0
+# for i in letra:
+#     print(i)
+#     less=less+1
+#     if i in "aeiouAEIOU":
+#         vocales=vocales+1
+#     elif i==" ":
+#         ''''''
+#     else:
+#         lolos=lolos+1
+        
+# print(f"este es el total de letras {less} y de vocales {vocales} y este es el total de consonantes {lolos}")
+          
+
+
+
+
+
